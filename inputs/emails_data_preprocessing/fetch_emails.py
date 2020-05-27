2d320c267ac9d08067c722a7d3fbb486bf3a0dc4
Tomas Javurek <tomasjavurek09@gmail.com>
2020-05-27 15:17:43 +0200

    local fetch of the emails

--- inputs/emails_data_preprocessing/fetch_emails.py
+++ inputs/emails_data_preprocessing/fetch_emails.py
@@ -7,6 +7,7 @@ import hashlib
 import update_db
 
 from copy import deepcopy
+from NER_name_tagger import remove_names
 
 detach_dir = '/attachments'
 
@@ -48,7 +49,7 @@ def _remove_names(text):
         names[str(person)] = name_hash
         full_name = person.split(' ')
         first_name = full_name[0]
-        surname = full_name[1]
+        surname = full_name[-1]
         if len(full_name) > 2:
             names[first_name+' '+surname] = name_hash
         names[surname] = name_hash
@@ -82,7 +83,40 @@ def populate_db(row):
     update_db.add_row(row)
 
 
-def search_fetch(server, uname, pwd, N=10000):
+def process_email(item, mail=None):
+
+    email_message = None
+    if mail:
+        _fetch, email_data = mail.uid('fetch', item, '(RFC822)')
+        raw_email = email_data[0][1].decode("UTF-8")
+        email_message = email.message_from_string(raw_email)
+    else:
+        email_message = item
+    body = email_message.get_payload(decode=True)
+    if body:
+        text = html_to_raw(body)
+        names, corrected_text = remove_names(text)
+        _, subject = remove_names(email_message['Subject'])
+        _, sender = remove_names('from ' + email_message['From'])
+        _, receiver = remove_names('to ' + email_message['To'])
+
+        # some hard-coded filter
+        # if 'atlas-adc-ddm-support' not in receiver: raise
+
+        print('\n')
+        print('############### NEW EMAIL #################')
+        print('SBJECT: {}'.format(subject))
+        print('FROM: {}'.format(sender))
+        print('TO {}'.format(receiver))
+        print('THREAD INDEX {}'.format(email_message['Thread-Index']))
+        print('DATE: {}'.format(email_message['Date']))
+        # print('EMAIL BODY:')
+        # print(corrected_text)
+        row_in_db = (sender, receiver, subject, email_message['Date'], email_message['Thread-Index'], corrected_text)
+        populate_db(row_in_db)
+
+
+def search_fetch_server(server, uname, pwd, N=1000):
     username = uname
     password = pwd
     mail = imaplib.IMAP4_SSL(server)
@@ -90,36 +124,15 @@ def search_fetch(server, uname, pwd, N=10000):
     mail.select("inbox")
     counter = 0
     try:
-        _search, data = mail.uid('search', None, '(ALL)')  # FLAGGED, '(RECENT)'
+        _search, data = mail.uid('search', None, '(ALL)')  # FLAGGED, '(RECENT)' (TO "atlas-adc-ddm-support@cern.ch")
         data_list = data[0].split()
         print('Found {} emails.'.format(len(data_list)))
         lmt = min(len(data_list), N)
         inbox_item_list = data_list[-lmt:]
         for item in inbox_item_list:
             try:
-                _fetch, email_data = mail.uid('fetch', item, '(RFC822)')
-                raw_email = email_data[0][1].decode("UTF-8")
-                email_message = email.message_from_string(raw_email)
-                body = email_message.get_payload(decode=True)
-                if body:
-                    print('\n')
-                    print('############### NEW EMAIL #################')
-                    text = html_to_raw(body)
-                    names, corrected_text = _remove_names(text)
-                    _, subject = _remove_names(email_message['Subject'])
-                    _, sender = _remove_names('From ' + email_message['From'])
-                    _, receiver = _remove_names('To ' + email_message['To'])
-
-                    print('SBJECT: {}'.format(subject))
-                    print('FROM: {}'.format(sender))
-                    print('TO {}'.format(receiver))
-                    print('THREAD INDEX {}'.format(email_message['Thread-Index']))
-                    print('DATE: {}'.format(email_message['Date']))
-                    # print('EMAIL BODY:')
-                    # print(corrected_text)
-                    row_in_db = (sender, receiver, subject, email_message['Date'], email_message['Thread-Index'], corrected_text)
-                    populate_db(row_in_db)
-                    counter += 1
+                process_email(item, mail)
+                counter += 1
             except Exception as e:
                 print('Not expected error:')
                 print(str(e))
@@ -131,14 +144,33 @@ def search_fetch(server, uname, pwd, N=10000):
     print('{} emails populate in db'.format(counter))
 
 
-login, pw = "ddmalerts", "passw"
+def search_fetch_local(inbox, N=100000):
+    import emlx
+    import glob
+    counter = 0
+    for filepath in glob.iglob(inbox + '/**', recursive=True):
+        if counter > N: break
+        if filepath.endswith('.emlx'):
+            counter += 1
+            try:
+                message = emlx.read(filepath)
+                process_email(message)
+                # print(message.get_payload(decode=True))
+                # print(message._payload)
+            except Exception as e:
+                print(str(e))
+    print('Fetched messages {}'.format(counter))
+
+
+login, pw = "ddmalerts", ""
 
 lmt = 1
 counter = 0
 while counter<lmt:
      counter += 1
      try:
-         search_fetch("imap.cern.ch", login, pw)
+         # search_fetch_server("imap.cern.ch", login, pw)
+         search_fetch_local('DDM.mbox')
          # names, text = _remove_names('The question is whether Tomas Javurek is a developer.')
          # print(names, text)
      except Exception as e:
