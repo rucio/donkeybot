09fe38c00b8e093ecb2a113016c18e7c0939f020
mageirakos <up1053601@upnet.gr>
2020-06-29 18:38:42 +0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bot',
+    version='0.0.1',
+    description="Rucio Support Bot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Vasilis Mageirakos',
+    packages=find_packages(),
+    install_requires=[
+          'pandas',
+          'numpy',
+          'nltk'
+      ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English',
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
