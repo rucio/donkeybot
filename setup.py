11fcccafe2880de198806d9a0e673ff059fef385
mageirakos <up1053601@upnet.gr>
2020-07-09 15:08:58 +0300

    update install_requires

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(
     install_requires=[
           'pandas',
           'numpy',
-          'nltk'
+          'nltk',
+          'rank_bm25'
       ],
     classifiers=[
         'Development Status :: 3 - Alpha',
