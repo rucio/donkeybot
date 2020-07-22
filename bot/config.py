a6d6773baf907df03e11f93c27f3ca2c509a0ac0
mageirakos <up1053601@upnet.gr>
2020-07-22 16:40:35 +0300

    add regex patterns for daemons

--- bot/config.py
+++ bot/config.py
@@ -28,18 +28,23 @@ try:
 except:
         CONVERSATION_DICT = {}
 
+# REGEX PATTERNS
+
 # removed for conversation creation when searching same subjects
 REGEX_METACHARACTERS = '^$.|?*+(){}[]'
 
-# reply emails quoting past emails
+# reply emails quoting past emails (EmailParser)
 ON_HDR_REGEX = re.compile(r'On (.*?)wrote: ', re.IGNORECASE)
 QUOTED_REGEX = re.compile(r'(>>+|> >+)')
 HEADER_REGEX = re.compile(r'(([_-]{7,})?(\s)From:(.*)(Sent:)?(.*)To:(.*)Subject:)', re.IGNORECASE)
 ORIGINAL_MSG_REGEX = re.compile(r'-----Original Message-----', re.IGNORECASE)
-
 # url
 URL_REGEX = re.compile(r'(https|http|www)[^ ]*')
 
-# emails sent automatically : find what other patterns exist in those
-# AUTOMATIC_MSG = re.compile(r'\\-- THIS IS AN AUTOMATICALLY GENERATED MESSAGE')
+# (RucioDocsFetcher)
+DAEMON_DOC_ARGS_REGEX = re.compile(r'..\sargparse::[\n\r\s]+:filename:\s(.*)[\n\r\s]+:func:\s(.*)[\n\r\s]+:prog:\s(.*)')
+# matches any string after inside ArgumentParser description for both types of quotes
+DAEMON_DESC_REGEX = re.compile(r'''ArgumentParser\(description=(?:("[^"]*"|'[^']*'))''', re.DOTALL)
+# matches the text inside epilog (which is in ArgumentParser) for both types of quotes
+DAEMON_EPILOG_REGEX = re.compile(r'''epilog=['|"]{3}(.*)['|"]{3}''', re.DOTALL)
 
