f09cfe07e3a1e2e35755f3f0c32e445d8a88cca0
mageirakos <up1053601@upnet.gr>
2020-08-08 16:21:15 +0300

    add user mention regex

--- bot/config.py
+++ bot/config.py
@@ -14,13 +14,13 @@ ON_HDR_REGEX = re.compile(r'On (.*?)wrote: ', re.IGNORECASE)
 QUOTED_REGEX = re.compile(r'(>>+|> >+)')
 HEADER_REGEX = re.compile(r'(([_-]{7,})?(\s)From:(.*)(Sent:)?(.*)To:(.*)Subject:)', re.IGNORECASE)
 ORIGINAL_MSG_REGEX = re.compile(r'-----Original Message-----', re.IGNORECASE)
-# url
-URL_REGEX = re.compile(r'(https|http|www)[^ ]*')
-
 # (RucioDocsFetcher)
 DAEMON_DOC_ARGS_REGEX = re.compile(r'..\sargparse::[\n\r\s]+:filename:\s(.*)[\n\r\s]+:func:\s(.*)[\n\r\s]+:prog:\s(.*)')
 # matches any string after inside ArgumentParser description for both types of quotes
 DAEMON_DESC_REGEX = re.compile(r'''ArgumentParser\(description=(?:("[^"]*"|'[^']*'))''', re.DOTALL)
 # matches the text inside epilog (which is in ArgumentParser) for both types of quotes
 DAEMON_EPILOG_REGEX = re.compile(r'''epilog=['|"]{3}(.*)['|"]{3}''', re.DOTALL)
+# (Other)
+URL_REGEX = re.compile(r'(https|http|www)[^ ]*')
+USER_MENTION_REGEX = re.compile(r'@[^\s]*')
 
