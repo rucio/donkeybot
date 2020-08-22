8da5f471546647ef1d314eaa15887241e7b89329
mageirakos <up1053601@upnet.gr>
2020-08-22 17:47:42 +0300

    add gui window for indexing new faqs; relates #26

--- lib/bot/faq/gui.py
+++ lib/bot/faq/gui.py
@@ -1,10 +1,11 @@
 # bot modules
 from bot.database.sqlite import Database
 from bot.faq.base import FAQ
+from bot.searcher.faq import FAQSearchEngine
 
 # general python
 import tkinter
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import abc
 import webbrowser
 
@@ -25,7 +26,7 @@ class Menubar(ttk.Frame):
     # TODO add FAQ documentation link when available
     def display_help(self):
         """Displays help document"""
-        webbrowser.open('https://github.com/rucio/donkeybot')
+        webbrowser.open("https://github.com/rucio/donkeybot")
         return
 
     def init_menubar(self):
@@ -82,8 +83,88 @@ class Window(ttk.Frame):
         self.parent.destroy()
 
 
+class IndexWindow(Window):
+    """ Index FAQ window """
+
+    def init_gui(self):
+        self.large_font = ("Verdana", 30)
+        self.small_font = ("Verdana", 10)
+
+        self.parent.title("FAQ Indexing")
+        self.parent.columnconfigure(0, weight=1)
+        self.parent.rowconfigure(3, weight=1)
+
+        # Create Widgets
+        self.label_title = ttk.Label(
+            self.parent, text="Use this to index FAQs for the Search Engine!"
+        )
+        self.contentframe = ttk.Frame(self.parent, relief="sunken")
+
+        self.db_label = ttk.Label(self.contentframe, text="Database:")
+        self.db_input = ttk.Entry(
+            self.contentframe,
+            width=50,
+            font=self.small_font,
+            validate="focusout",
+            validatecommand=(self.validate_notempty),
+        )
+        self.db_input.insert(0, "data_storage")
+
+        self.table_label = ttk.Label(self.contentframe, text="FAQ table:")
+        self.table_input = ttk.Entry(
+            self.contentframe,
+            width=50,
+            font=self.small_font,
+            validate="focusout",
+            validatecommand=(self.validate_notempty),
+        )
+        self.table_input.insert(0, "faq")
+
+        self.btn_do = ttk.Button(
+            self.parent, text="Create index", command=self.on_click
+        )
+        self.btn_cancel = ttk.Button(self.parent, text="Cancel", command=self.close_win)
+
+        # Layout
+        self.label_title.grid(row=0, column=0, columnspan=4, sticky="nsew")
+        self.contentframe.grid(row=1, column=0, columnspan=4, sticky="nsew")
+
+        self.db_label.grid(row=0, column=0)
+        self.db_input.grid(row=0, column=1, sticky="w")
+
+        self.table_label.grid(row=1, column=0)
+        self.table_input.grid(row=1, column=1, sticky="w")
+
+        self.btn_do.grid(row=2, column=0, sticky="e")
+        self.btn_cancel.grid(row=2, column=1, sticky="e")
+
+        # Padding
+        for child in self.parent.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+        for child in self.contentframe.winfo_children():
+            child.grid_configure(padx=5, pady=2)
+
+    def on_click(self):
+        """Indexes FAQ values for the Search Engine"""
+        db_name = self.db_input.get().strip()
+        faq_table_name = self.table_input.get().strip()
+        if db_name and faq_table_name:
+            data_storage = Database(f"{db_name}.db")
+            faq_df = data_storage.get_dataframe(table=f"{faq_table_name}")
+            faq_se = FAQSearchEngine()
+            faq_se.create_index(
+                corpus=faq_df,
+                db=data_storage,
+                table_name=f"{faq_table_name}_doc_term_matrix",
+            )
+            data_storage.close_connection()
+            self.close_win()
+        else:
+            print("Error: But for real though, fields must not be empty.")
+
+
 class QAPairWindow(Window):
-    """ New popup window """
+    """ QA Pair window """
 
     def init_gui(self):
 
@@ -154,7 +235,7 @@ class QAPairWindow(Window):
             validatecommand=(self.validate_notempty),
         )
 
-        self.btn_do = ttk.Button(self.parent, text="Action", command=self.on_click)
+        self.btn_do = ttk.Button(self.parent, text="Insert FAQ", command=self.on_click)
         self.btn_cancel = ttk.Button(self.parent, text="Cancel", command=self.close_win)
 
         # Layout
@@ -202,7 +283,6 @@ class QAPairWindow(Window):
                 db_name, faq_table_name, question, answer, author, keywords
             )
             self.close_win()
-            pass
         else:
             print("Error: But for real though, field must not be empty.")
 
@@ -223,6 +303,7 @@ class QAPairWindow(Window):
         )
         data_storage.insert_faq(faq_obj, table_name=faq_table_name)
         print(f"FAQ object inserted in '{faq_table_name}' table on {db_name}.db!")
+        data_storage.close_connection()
 
 
 class GUI(ttk.Frame):
@@ -232,17 +313,23 @@ class GUI(ttk.Frame):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
         self.root = parent
         self.init_gui()
+        self.large_font = ("Verdana", 30)
 
     def openwindow(self):
         self.new_win = tkinter.Toplevel(self.root)  # Set parent
         QAPairWindow(self.new_win)
 
+    def open_index_window(self):
+        self.new_win = tkinter.Toplevel(self.root)  # Set parent
+        IndexWindow(self.new_win)
+
     def init_gui(self):
-        self.root.title("Test GUI")
-        self.root.geometry("600x400")
+        self.root.title("FAQ GUI")
+        self.root.geometry("600x200")
         self.grid(column=0, row=0, sticky="nsew")
         self.grid_columnconfigure(0, weight=1)  # Allows column to stretch upon resizing
         self.grid_rowconfigure(0, weight=1)  # Same with row
+        self.grid_rowconfigure(1, weight=1)  # Same with second row
         self.root.grid_columnconfigure(0, weight=1)
         self.root.grid_rowconfigure(0, weight=1)
         self.root.option_add(
@@ -252,18 +339,34 @@ class GUI(ttk.Frame):
         # Menu Bar
         self.menubar = Menubar(self.root)
 
-        # Create Widgets
-        self.btn = ttk.Button(self, text="Open Window", command=self.openwindow)
+        # Insert new FAQ Widget
+        self.faq_btn = ttk.Button(self, text="Insert new FAQ", command=self.openwindow)
+        # Index FAQs Widget
+        self.idx_btn = ttk.Button(
+            self,
+            text="Index FAQ table for the Search Engine",
+            command=self.open_index_window,
+        )
 
         # Layout using grid
-        self.btn.grid(row=0, column=0, sticky="ew")
+        self.faq_btn.grid(row=0, column=0, sticky="ew")
+        self.idx_btn.grid(row=1, column=0, sticky="ew")
 
         # Padding
         for child in self.winfo_children():
-            child.grid_configure(padx=10, pady=5)
+            child.grid_configure(padx=1, pady=1)
+
+
+def on_closing():
+    if messagebox.askokcancel(
+        "Quit", "Did you remember to index the new FAQs for the Search Engine?"
+    ):
+        root.destroy()
 
 
 if __name__ == "__main__":
     root = tkinter.Tk()
     GUI(root)
+    # make sure to re-index FAQs for the Search Engine before closing the window
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
