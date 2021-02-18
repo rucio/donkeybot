fc5618d2bac09c165d9dab989283833e1c671f3e
mageirakos <up1053601@upnet.gr>
2021-02-18 22:00:25 +0200

    query faqs solo;remove self.query

--- lib/bot/brain.py
+++ lib/bot/brain.py
@@ -42,17 +42,19 @@ class QAInterface:
                 "Error: Wrong search engine type. Make sure to use one of DonkeyBot's Search Engines."
             )
 
-    def _get_faq_answers(self, num_faqs):
+    def get_faq_answers(self, query, num_faqs):
         """
-        Returns the most similar answers from FAQ table.
+        Returns num_faqs most similar answers from FAQ table.
 
         <!> Note: AnswerDetector is not needed when matching
         through the FAQ table. The only important thing is
         to find the most similar questions.
-
+        
+        :param query         : User's question/query 
+        :param num_faqs      : Number of retrieved FAQ answers (default is 3)
         :return faq_answers : list of Answer objects
         """
-        self.retrieved_faqs = self.faq_engine.search(self.query, num_faqs)
+        self.retrieved_faqs = self.faq_engine.search(query, num_faqs)
         faq_answers = []
         for _, faq in self.retrieved_faqs.iterrows():
             metadata = (
@@ -61,7 +63,7 @@ class QAInterface:
                 .to_dict()
             )
             answer = Answer(
-                question=self.query,  # what the user asked
+                question=query,  # what the user asked
                 model="FAQSearchEngine",  # Answer returns is retrieved FAQ so no transformer used
                 answer=faq["answer"],
                 start=0,
@@ -75,50 +77,46 @@ class QAInterface:
             faq_answers.append(answer)
         return faq_answers
 
-    def _get_question_answers(self, num_questions):
-        """Returns top_k answers based on the most similar questions and their context."""
+    def _get_question_answers(self, query, num_questions):
+        """Returns num_questions answers based on the most similar questions and their context."""
         # most similar questions
         self.retrieved_questions = self.question_engine.search(
-            self.query, num_questions
+            query, num_questions
         )
         question_answers = self.detector.predict(
-            self.query, self.retrieved_questions, top_k=self.top_k
+            query, self.retrieved_questions, top_k=self.top_k
         )
         return question_answers
 
-    def _get_docs_answers(self, num_docs):
-        """Returns top_k answers based on the most similar documentation."""
+    def _get_docs_answers(self, query, num_docs):
+        """Returns num_docs answers based on the most similar documentation."""
         # most similar documentation docs
-        self.retrieved_docs = self.docs_engine.search(self.query, num_docs)
+        self.retrieved_docs = self.docs_engine.search(query, num_docs)
         doc_answers = self.detector.predict(
-            self.query, self.retrieved_docs, top_k=self.top_k
+            query, self.retrieved_docs, top_k=self.top_k
         )
         return doc_answers
 
-    def get_answers(self, query, top_k=3, num_faqs=3, num_questions=10, num_docs=10):
+    def get_answers(self, query, top_k=3, num_questions=10, num_docs=10):
         """
         Return top_k number of Answers based on user query.
 
-        :param query         : User's question/query
+        :param query         : User's question/query (passed on to private functions)
         :param top_k         : Number of Answers returned (default is 3)
-        :param num_faqs      : Number of retrieved FAQ answers (default is 3)
         :param num_questions : Number of retrieved Questions (default is 10)
         :param num_docs      : Number of retrieved Documents (default is 10)
         :returns answers     : List of top_k Answer objects + num_faq Answer objects from FAQ
         """
-        self.query = query
+        # self.query = query
         self.top_k = top_k
 
         # extract answers
-        self.faq_answers = self._get_faq_answers(num_faqs)
-        self.question_answers = self._get_question_answers(num_questions)
-        self.doc_answers = self._get_docs_answers(num_docs)
+        self.question_answers = self._get_question_answers(query, num_questions)
+        self.doc_answers = self._get_docs_answers(query, num_docs)
 
         # sort answers by their `confidence` and select top-k from question/doc answers
         self.answers = self.question_answers + self.doc_answers
         self.answers = sorted(self.answers, key=lambda k: k.confidence, reverse=True)
         self.answers = self.answers[:top_k]
 
-        # concat the faq answers
-        self.final_answers = self.answers + self.faq_answers
-        return self.final_answers
+        return self.answers
